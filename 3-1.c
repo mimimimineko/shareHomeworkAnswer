eccaaf9ea3e5b67a0ad741a419bf58017b32c8dc
mimimimineko <80242131+mimimimineko@users.noreply.github.com>
2022-01-21 14:11:56 +0900

    Add files via upload

---
+++ 3-1.c
@@ -0,0 +1,13 @@
+﻿#include<stdio.h>
+int main (void){
+    int count=0;
+    int ch;
+    printf("入力↓↓↓\n");
+    while((ch=getchar()) != EOF ){
+        if(ch=='\n'){
+            count ++;
+        }
+    }3-1
+    printf("%d\n",count);
+    return 0;
+}
---
+++ 4.c
@@ -0,0 +1,10 @@
+﻿#include<stdio.h>
+#include<math.h>
+
+int main(void){
+    printf("一辺の長さを求めたい正方形の面積を入力せよ。");
+    double a;
+    scanf("%lf",&a);
+    printf("%f\n",sqrt(a));
+    return 0;
+}
