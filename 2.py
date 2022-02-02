3571021347c75bd0203d94ccfeaee22fd81107ec
mimimimineko <80242131+mimimimineko@users.noreply.github.com>
2022-02-02 11:00:45 +0900

    Delete 2.py

--- 2.py
+++
@@ -1,9 +0,0 @@
-﻿print("二つの整数を入力してください。")
-a=int (input("整数 A: "))
-b=int (input("整数 B: "))
-if a>b:
-    print("A は B より大きいです。")
-elif a==b:
-    print("A と B は等しいです。")
-else:
-    print("A は B より小さいです。")
